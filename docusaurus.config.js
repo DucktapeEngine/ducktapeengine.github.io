049e6c74502c8e21318163930709ad8314efb6ce
Duckie <aryanbaburajan2007@gmail.com>
2022-01-25 20:34:21 +0530

    asd

---
+++ docusaurus.config.js
@@ -0,0 +1,132 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/* @type {import('@ducktape/types').Config} */
+const config = {
+	title: 'Ducktape',
+	tagline: 'An open source 2d game engine that makes sure your game stands out.',
+	url: 'https://ducktapeengine.github.io',
+	baseUrl: '/',
+	onBrokenLinks: 'warn',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'DucktapeEngine', // Usually your GitHub org/user name.
+	projectName: 'DucktapeEngine.github.io', // Usually your repo name.
+	deploymentBranch: 'main',
+	presets: [
+		[
+			'classic',
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					editUrl: 'https://github.com/ducktapeengine/ducktape/tree/main/packages/create-ducktape/templates/shared/',
+				},
+				blog: {
+					showReadingTime: true,
+					editUrl:
+						'https://github.com/ducktapeengine/ducktape/tree/main/packages/create-ducktape/templates/shared/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
+
+	themeConfig:
+		({
+			navbar: {
+				title: 'Ducktape',
+				logo: {
+					alt: 'My Site Logo',
+					src: 'img/logo.svg',
+				},
+				items: [
+					{
+						type: 'doc',
+						docId: 'intro',
+						position: 'left',
+						label: 'Manual',
+					},
+					{ to: '/blog', label: 'Blog', position: 'left' },
+					{
+						href: 'https://github.com/ducktapeengine/ducktape',
+						label: 'GitHub',
+						position: 'right',
+					}
+				],
+				
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Docs',
+						items: [
+							{
+								label: 'Manual',
+								to: '/Manual/Intro',
+							},
+							{
+								label: 'Scripting Reference',
+								to: '/ScriptingReference',
+							}
+						],
+					},
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'StackOverflow',
+								href: 'https://stackoverflow.com/questions/tagged/ducktapeengine',
+							},
+							{
+								label: 'Discord',
+								href: 'https://dsc.gg/ducktape',
+							}
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'Blog',
+								to: '/blog',
+							},
+							{
+								label: 'GitHub',
+								href: 'https://github.com/ducktapeengine/ducktape',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Ducktape, Inc.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+
+			colorMode: {
+				defaultMode: 'dark',
+				disableSwitch: true,
+				respectPrefersColorScheme: false,
+			},
+
+			image: 'img/ducktape.png',
+
+			announcementBar: {
+				id: 'support_us',
+				content:
+				  'Like what you see? Help us by <a target="_blank" rel="noreferrer noopener" href="https://github.com/ducktapeengine/ducktape">starring the repository!</a>',
+				backgroundColor: '#343434',
+				textColor: '#FED900',
+				isCloseable: true,
+			  },
+		}),
+};
+
+module.exports = config;
---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <br></br>
+        <a id={styles.getStarted} href="docs/intro"> Get started </a>
+      </div>
+      <img id={styles.curve} src="./img/curve.svg"/>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Home`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
