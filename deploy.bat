b104f3432e99d972636a8b75f9a53357e7e90b66
Duckie <aryanbaburajan2007@gmail.com>
2022-01-28 09:24:47 +0530

    add scriptingreference

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,5 @@
 git add .
 git commit -m "deploy"
 git push
+cls
+echo "Deployed, the site should be live at https://ducktapeengine.github.io/ in a couple of minutes."
---
+++ static/scriptingreference/ducktape_8h.js
@@ -0,0 +1,4 @@
+var ducktape_8h =
+[
+    [ "init", "ducktape_8h.html#a8cb027ab8529865935bfd623f84d6001", null ]
+];
