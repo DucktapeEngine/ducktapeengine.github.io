d5657de7cac936cfbb6dfcbef141106fdfc13a78
TheDuckDev <aryanbaburajan2007@gmail.com>
2022-01-28 03:56:31 +0000

    Deploying to gh-pages from @ DucktapeEngine/ducktapeengine.github.io@b104f3432e99d972636a8b75f9a53357e7e90b66 🚀

---
+++ scriptingreference/class_d_t_1_1_transform.js
@@ -0,0 +1,19 @@
+var class_d_t_1_1_transform =
+[
+    [ "getLocalPosition", "class_d_t_1_1_transform.html#a14c0d3fe7576d4b63d305b396caa88ba", null ],
+    [ "getLocalRotation", "class_d_t_1_1_transform.html#aa6083168f78aa3a708e910f729c88e6c", null ],
+    [ "getLocalScale", "class_d_t_1_1_transform.html#a0aed044ecfebd84ec3a4a43d4ea6faa7", null ],
+    [ "getPosition", "class_d_t_1_1_transform.html#a25c370710bc40f72e181e84bca8132b2", null ],
+    [ "getRotation", "class_d_t_1_1_transform.html#a5abeb1ff2da5dc4a3517715b85881f7c", null ],
+    [ "getScale", "class_d_t_1_1_transform.html#af582a129cef9423a64b068925d385308", null ],
+    [ "recalculateLocalValues", "class_d_t_1_1_transform.html#a29e7af31510ebf4f47fdafce83f4f3fa", null ],
+    [ "recalculateWorldValues", "class_d_t_1_1_transform.html#af66746d24114e26c3ba697a83360f3ab", null ],
+    [ "setLocalPosition", "class_d_t_1_1_transform.html#a52a91c8c9d907bed52881ae1647cca92", null ],
+    [ "setLocalRotation", "class_d_t_1_1_transform.html#aa1de82d7042bae893867c05162759fd4", null ],
+    [ "setLocalScale", "class_d_t_1_1_transform.html#acea5273635cfa1611192bfa0ba0a2849", null ],
+    [ "setPosition", "class_d_t_1_1_transform.html#a881b19f5e6f0e0759fc89e67a71956c9", null ],
+    [ "setRotation", "class_d_t_1_1_transform.html#a0bc2001670e413b04b5f45a4aec3a5ad", null ],
+    [ "setScale", "class_d_t_1_1_transform.html#a1aac79e1b6a0c3f306c982969bb5bcef", null ],
+    [ "children", "class_d_t_1_1_transform.html#a7133bdff7e0623c4ec5fe74bff51e1d8", null ],
+    [ "parent", "class_d_t_1_1_transform.html#a5cefda0110d926e5859dfb464d18488f", null ]
+];
---
+++ scriptingreference/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -0,0 +1,4 @@
+var dir_d44c64559bbebec7f509842c48db8b23 =
+[
+    [ "Ducktape", "dir_e93a7d41a3b3b2c1eb98c2b33a032545.html", "dir_e93a7d41a3b3b2c1eb98c2b33a032545" ]
+];
