82ddecbdae29878ae432c20a909d92e4030f3961
Duckie <aryanbaburajan2007@gmail.com>
2022-01-28 10:15:26 +0530

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,1 @@
-git add .
-git commit -m "deploy"
-git push
-cls
-echo "Deployed, the site should be live at https://ducktapeengine.github.io/ in a couple of minutes."
+git add . & git commit -m "deploy" & git push & cls & echo "Deployed, the site should be live at https://ducktapeengine.github.io/ in a couple of minutes."
