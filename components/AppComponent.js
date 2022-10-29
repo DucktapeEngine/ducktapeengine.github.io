3562a1f26c8040d787523d033eed266af00054c9
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-10-30 01:34:51 +0800

    add notes on browser api and running

--- components/AppComponent.js
+++ components/AppComponent.js
@@ -3,6 +3,11 @@
 
 const e = React.createElement;
 
+// it took me some time to figure out how this e() function works so heres how it works:
+// e('name of tag', { events: callbacks }, 'value')
+// to run the site locally you can just use the Live Serve VSCode extension, and run the index.html file.
+// enjoy
+
 class AppComponent extends React.Component {
   constructor(props) {
     super(props);
